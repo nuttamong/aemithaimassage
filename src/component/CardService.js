2d6cd338f686b23eac46f8c00476651bfeb5da4d
nuttamong <nuttamong@kkumail.com>
2024-12-26 12:13:56 +0700

    edit

--- src/component/CardService.js
+++ src/component/CardService.js
@@ -6,7 +6,7 @@ function CardService(props) {
     const prices = props.prices.map((price) => <Card.Text key={id++}>{price}</Card.Text>);
     return (
         <Card style={{ minWidth: '25rem', minHeight: '30rem' }} className='my-3'>
-            <Card.Img variant="top" src={props.image} style={{ height: '20rem', width: '29rem'}}/>
+            <Card.Img variant="top" src={props.image} style={{ maxHeight: '20rem', maxWidth: '29rem'}}/>
             <Card.Body>
                 <Card.Title>{props.nameService}</Card.Title>
                 {prices}
